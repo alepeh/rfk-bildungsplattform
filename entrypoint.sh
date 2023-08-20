618f2d8ec9acd4a9c48b2379f720c9e8004b4d44
Alexander Pehm <alexander@pehm.biz>
2023-08-20 12:23:16 +0200

    Fixed gunicorn command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RUN_PORT="8001"
 
-/opt/venv/bin/gunicorn bildungsplattform.wsgi.application --bind "0.0.0.0:${RUN_PORT}" --daemon
+/opt/venv/bin/gunicorn bildungsplattform.wsgi:application --bind "0.0.0.0:${RUN_PORT}" --daemon
 
 nginx -g 'daemon off;'
