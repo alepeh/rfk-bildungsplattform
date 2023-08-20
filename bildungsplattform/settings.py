0212e316093f1d351f339a276abb025109d81c73
Alexander Pehm <alexander@pehm.biz>
2023-08-20 09:02:30 +0200

    turn on debug

--- bildungsplattform/settings.py
+++ bildungsplattform/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 
