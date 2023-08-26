6a44ccfbd8deadea5a5244a35d402f7ec49824dd
Alexander Pehm <alexander@pehm.biz>
2023-08-26 12:26:21 +0200

    Enhanced core model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from core.models import Funktion, Person
+from core.models import Funktion, Person, Schulung, SchulungsArt, Betrieb
+
 
 class FunktionAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 admin.site.register(Funktion, FunktionAdmin)
 admin.site.register(Person)
+admin.site.register(Schulung)
+admin.site.register(SchulungsArt)
+admin.site.register(Betrieb)
--- core/models.py
+++ core/models.py
@@ -2,15 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Funktion(models.Model):
+class SchulungsArt(models.Model):
     name = models.CharField(max_length=100)
 
+
+class Schulung(models.Model):
+    name = models.CharField(max_length=100)
+    art = models.OneToOneField(to=SchulungsArt, on_delete=models.CASCADE)
+
+
+class Funktion(models.Model):
+    name = models.CharField(max_length=100)
+    schulungsanforderung = models.ManyToManyField(
+        Schulung,
+        through='FunktionSchulung',
+        through_fields=('funktion', 'schulung'),
+    )
     def __str__(self):
         return str(self.name)
 
     class Meta:
         ordering = ['name']
         verbose_name_plural = "funktionen"
+
 
 class Person(models.Model):
     benutzer = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
@@ -24,3 +38,22 @@ class Person(models.Model):
     class Meta:
         ordering = ['nachname']
         verbose_name_plural = "personen"
+
+
+class Betrieb(models.Model):
+    name = models.CharField(max_length=100)
+    inhaber = models.OneToOneField(Person, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "betriebe"
+
+
+class FunktionSchulung(models.Model):
+    schulung = models.OneToOneField(to=Schulung, on_delete=models.CASCADE)
+    funktion = models.OneToOneField(to=Funktion, on_delete=models.CASCADE)
+    intervall = models.IntegerField()
+
