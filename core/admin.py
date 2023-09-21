40eb4e09b5039ec86f6a27cce9bb4601f5b5fba5
Alexander Pehm <alexander.pehm@paysafe.com>
2023-09-21 17:14:57 +0200

    Enhanced data model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from core.models import Funktion, Person, Schulung, SchulungsArt, Betrieb, SchulungsArtFunktion, SchulungsTermin, \
-    SchulungsOrt
+    SchulungsOrt, SchulungsTerminPerson
 
 class PersonInline(admin.TabularInline):
     model = Person
@@ -9,6 +9,18 @@ class PersonInline(admin.TabularInline):
 
 class SchulungsArtFunktionInline(admin.TabularInline):
     model = SchulungsArtFunktion
+    extra = 1
+
+class SchulungsTerminInline(admin.TabularInline):
+    model = SchulungsTermin
+    extra = 1
+
+class SchulungAdmin(admin.ModelAdmin):
+    model = Schulung
+    inlines = (SchulungsTerminInline,)
+
+class SchulungsTerminPersonInline(admin.TabularInline):
+    model = SchulungsTerminPerson
     extra = 1
 
 
@@ -28,13 +40,15 @@ class PersonAdmin(admin.ModelAdmin):
 
 class SchulungsTerminAdmin(admin.ModelAdmin):
     list_display = ('schulung', 'datum')
+    inlines = (SchulungsTerminPersonInline,)
+
 
 class BetriebAdmin(admin.ModelAdmin):
     inlines = [PersonInline,]
 
 admin.site.register(Funktion, FunktionAdmin)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Schulung)
+admin.site.register(Schulung, SchulungAdmin)
 admin.site.register(SchulungsArt)
 admin.site.register(Betrieb, BetriebAdmin)
 admin.site.register(SchulungsOrt)
--- core/models.py
+++ core/models.py
@@ -35,13 +35,14 @@ class SchulungsTermin(models.Model):
     datum = models.DateTimeField()
     beschreibung = models.TextField(max_length=200)
     ort = models.OneToOneField(to=SchulungsOrt, on_delete=models.DO_NOTHING)
-    schulung = models.OneToOneField(to=Schulung, on_delete=models.CASCADE)
+    schulung = models.ForeignKey(to=Schulung, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.datum)
 
     class Meta:
         verbose_name_plural = "Schulungstermine"
+
 
 class Funktion(models.Model):
     name = models.CharField(max_length=100)
@@ -57,6 +58,7 @@ class Funktion(models.Model):
         ordering = ['name']
         verbose_name_plural = "funktionen"
 
+
 class Betrieb(models.Model):
     name = models.CharField(max_length=100)
 
@@ -67,11 +69,12 @@ class Betrieb(models.Model):
         ordering = ['name']
         verbose_name_plural = "betriebe"
 
+
 class Person(models.Model):
     benutzer = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     vorname = models.CharField(max_length=150)
     nachname = models.CharField(max_length=150)
-    funktion = models.OneToOneField(Funktion, on_delete=models.PROTECT)
+    funktion = models.ForeignKey(Funktion, on_delete=models.PROTECT)
     betrieb = models.ForeignKey(Betrieb, on_delete=models.PROTECT, null=True)
     def __str__(self):
         return str(self.vorname + " " + self.nachname)
@@ -81,9 +84,29 @@ class Person(models.Model):
         verbose_name_plural = "personen"
 
 
+class SchulungsTerminPerson(models.Model):
+    schulungstermin = models.ForeignKey(to=SchulungsTermin, on_delete=models.CASCADE)
+    person = models.ForeignKey(to=Person, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('Angemeldet', 'Angemeldet'),
+        ('Teilgenommen', 'Teilgenommen'),
+        ('Entschuldigt', 'Entschuldigt'),
+        ('Unentschuldigt', 'Unentschuldigt'),
+    ]
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='Angemeldet',
+    )
+
+    class Meta:
+        verbose_name = "Teilnehmer"
+        verbose_name_plural = "Teilnehmer"
+
+
 class SchulungsArtFunktion(models.Model):
-    schulungsart = models.OneToOneField(to=SchulungsArt, on_delete=models.CASCADE)
-    funktion = models.OneToOneField(to=Funktion, on_delete=models.CASCADE)
+    schulungsart = models.ForeignKey(to=SchulungsArt, on_delete=models.CASCADE)
+    funktion = models.ForeignKey(to=Funktion, on_delete=models.CASCADE)
     intervall = models.IntegerField()
 
     def __str__(self):
