88617b0cfb01a79f83647d993c38681463f8ce70
Alexander Pehm <alexander@pehm.biz>
2023-12-22 07:48:29 +0100

    Additional details for schulungsort

--- core/models.py
+++ core/models.py
@@ -13,6 +13,12 @@ class SchulungsArt(models.Model):
 
 class SchulungsOrt(models.Model):
     name = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=100, null=True, blank=True)
+    plz = models.CharField(max_length=20, null=True, blank=True)
+    ort = models.CharField(max_length=50, null=True, blank=True)
+    kontakt = models.CharField(max_length=100, null=True, blank=True)
+    telefon = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
